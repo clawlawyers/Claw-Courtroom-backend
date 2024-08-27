a4388f81888dc37795d0f9e8729a4da0d6e71a8b
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-08-27 21:32:47 +0530

    end case api updated

--- src/controllers/coutroom-cotroller.js
+++ src/controllers/coutroom-cotroller.js
@@ -741,7 +741,7 @@ async function FetchRestCase(body) {
 }
 
 async function endCase(req, res) {
-  const user_id = req.body?.courtroomClient?.userBooking?.userId;
+  // const user_id = req.body?.courtroomClient?.userBooking?.userId;
   const { userId } = req.body;
   try {
     const endCase = await FetchEndCase({ userId });
