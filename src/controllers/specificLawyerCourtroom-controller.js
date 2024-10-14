158adbcef3623125cdf41af116291780adace416
rajkbharali <rajkbharali@gmail.com>
2024-10-14 15:37:09 +0530

    changes in routes

--- src/controllers/specificLawyerCourtroom-controller.js
+++ src/controllers/specificLawyerCourtroom-controller.js
@@ -1750,7 +1750,10 @@ async function relevantCaseLaw(req, res) {
 
   try {
     const relevantCases = await FetchRelevantCases({ user_id });
-    relevantCases = await encryption(relevantCases, key);
+    relevantCases.relevant_case_law = await encryption(
+      relevantCases.relevant_case_law,
+      key
+    );
     res.status(StatusCodes.OK).json(SuccessResponse({ relevantCases }));
   } catch (error) {
     console.error(error);
@@ -2112,7 +2115,10 @@ async function editApplication(req, res) {
     const { query } = req.body;
     const editApplication = await fetchEditApplication({ user_id, query });
 
-    editApplication = await encryption(editApplication, key);
+    editApplication.application = await encryption(
+      editApplication.application,
+      key
+    );
 
     res.status(StatusCodes.OK).json(SuccessResponse({ editApplication }));
   } catch (error) {
